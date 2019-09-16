0fb67e6b126821c070cba58b0c0837cd423343e7
Vladimir Moroz <vovanmoroz1509@gmail.com>
2019-09-16 09:30:14 +0300

    FreeRTOS updated

--- Lab3.1/Debug/sources.mk
+++ Lab3.1/Debug/sources.mk
@@ -2,27 +2,19 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-C_UPPER_SRCS := 
-CXX_SRCS := 
-C++_SRCS := 
 OBJ_SRCS := 
 S_SRCS := 
-CC_SRCS := 
 ASM_SRCS := 
-CPP_SRCS := 
 C_SRCS := 
 S_UPPER_SRCS := 
 O_SRCS := 
-CC_DEPS := 
-C++_DEPS := 
-EXECUTABLES := 
-C_UPPER_DEPS := 
-CXX_DEPS := 
 OBJS := 
-CPP_DEPS := 
+ARCHIVES := 
 C_DEPS := 
 
 # Every subdirectory with source files must be described here
 SUBDIRS := \
 src \
+src/portable/GCC/ARM_CM3 \
+src/portable/MemMang \
 
