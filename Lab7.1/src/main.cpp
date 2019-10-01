dfc929fabc00e489c26464d9b6abb4a3ac527e42
Vladimir Moroz <vovanmoroz1509@gmail.com>
2019-10-01 16:11:28 +0300

    Started work on lab 8

--- Lab7.1/src/main.cpp
+++ Lab7.1/src/main.cpp
@@ -6,10 +6,11 @@
  Copyright   : $(copyright)
  Description : main definition
 ===============================================================================
-*/
+ */
 
 #if defined (__USE_LPCOPEN)
 #if defined(NO_BOARD_LIB)
+
 #include "chip.h"
 #else
 #include "board.h"
@@ -17,32 +18,136 @@
 #endif
 
 #include <cr_section_macros.h>
+#include <stdlib.h>
+#include <time.h>
 
 // TODO: insert other include files here
-
+#include <string>
 // TODO: insert other definitions and declarations here
 
-int main(void) {
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+#include "queue.h"
+#include "sct_15xx.h"
+#include "sct_pwm_15xx.h"
+#include "swm_15xx.h"
+#include "ITM_write.h"
+#include "DigitalIoPin.h"
 
-#if defined (__USE_LPCOPEN)
-    // Read clock settings and update SystemCoreClock variable
-    SystemCoreClockUpdate();
-#if !defined(NO_BOARD_LIB)
-    // Set up and initialize all required blocks and
-    // functions related to the board hardware
-    Board_Init();
-    // Set the LED to the state of "On"
-    Board_LED_Set(0, true);
-#endif
-#endif
+/*****************************************************************************
+ * Private types/enumerations/variables
+ ****************************************************************************/
 
-    // TODO: insert code here
+/*****************************************************************************
+ * Public types/enumerations/variables
+ ****************************************************************************/
 
-    // Force the counter to be placed into memory
-    volatile static int i = 0 ;
-    // Enter an infinite loop, just incrementing a counter
-    while(1) {
-        i++ ;
-    }
-    return 0 ;
+/*****************************************************************************
+ * Private functions
+ ****************************************************************************/
+
+/* Sets up system hardware */
+void SCT_Init(void)
+{
+ LPC_SCT0->CONFIG |= (1 << 17); // two 16-bit timers, auto limit
+ LPC_SCT0->CTRL_L |= (12-1) << 5; // set prescaler, SCTimer/PWM clock = 1 MHz
+
+ LPC_SCT0->MATCHREL[0].L = 10*100-1; // match 0 @ 10/1MHz = 10 usec (100 kHz PWM freq)
+ LPC_SCT0->MATCHREL[1].L = 5; // match 1 used for duty cycle (in 10 steps)
+ LPC_SCT0->EVENT[0].STATE = 0xFFFFFFFF; // event 0 happens in all states
+ LPC_SCT0->EVENT[0].CTRL = (1 << 12); // match 0 condition only
+ LPC_SCT0->EVENT[1].STATE = 0xFFFFFFFF; // event 1 happens in all states
+ LPC_SCT0->EVENT[1].CTRL = (1 << 0) | (1 << 12); // match 1 condition only
+ LPC_SCT0->OUT[0].SET = (1 << 0); // event 0 will set SCTx_OUT0
+ LPC_SCT0->OUT[0].CLR = (1 << 1); // event 1 will clear SCTx_OUT0
+ LPC_SCT0->CTRL_L &= ~(1 << 2); // unhalt it by clearing bit 2 of CTRL reg
 }
+
+static void prvSetupHardware(void)
+{
+	SystemCoreClockUpdate();
+	Board_Init();
+	// initialize RIT (= enable clocking etc.)
+	Chip_RIT_Init(LPC_RITIMER);
+	Chip_SCT_Init(LPC_SCT0);
+	SCT_Init();
+	ITM_init();
+	// set the priority level of the interrupt
+	// The level must be equal or lower than the maximum priority specified in FreeRTOS config
+	// Note that in a Cortex-M3 a higher number indicates lower interrupt priority
+	NVIC_SetPriority( RITIMER_IRQn, configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY + 1 );
+	/* Initial LED0 state is off */
+	Board_LED_Set(0, false);
+}
+
+
+
+
+
+
+
+static void SCT_Task(void *pvParameters)
+{
+
+	DigitalIoPin sw1(0,17, DigitalIoPin::pullup, true);
+	DigitalIoPin sw2(1, 11, DigitalIoPin::pullup, true);
+	DigitalIoPin sw3(1, 9, DigitalIoPin::pullup, true);
+	Chip_SWM_MovablePortPinAssign(SWM_SCT0_OUT0_O, 0, 3);
+	while(1)
+	{
+		if(sw1.read())
+		{
+			LPC_SCT0->MATCHREL[1].L++;
+			vTaskDelay(10);
+
+		}
+		if(sw3.read())
+		{
+			LPC_SCT0->MATCHREL[1].L--;
+			vTaskDelay(10);
+		}
+
+
+	}
+}
+
+/*****************************************************************************
+ * Public functions
+ ****************************************************************************/
+
+/* the following is required if runtime statistics are to be collected */
+extern "C" {
+
+void vConfigureTimerForRunTimeStats( void ) {
+	Chip_SCT_Init(LPC_SCTSMALL1);
+	LPC_SCTSMALL1->CONFIG = SCT_CONFIG_32BIT_COUNTER;
+	LPC_SCTSMALL1->CTRL_U = SCT_CTRL_PRE_L(255) | SCT_CTRL_CLRCTR_L; // set prescaler to 256 (255 + 1), and start timer
+}
+
+}
+/* end runtime statictics collection */
+
+/**
+ * @brief	main routine for FreeRTOS blinky example
+ * @return	Nothing, function should not exit
+ */
+int main(void)
+{
+
+
+ 	prvSetupHardware();
+
+
+	xTaskCreate(SCT_Task, "SCT_Task",
+			configMINIMAL_STACK_SIZE + 128, NULL, (tskIDLE_PRIORITY + 1UL),
+			(TaskHandle_t *) NULL);
+
+
+	/* Start the scheduler */
+	vTaskStartScheduler();
+
+	/* Should never arrive here */
+	return 1;
+}
+
---
+++ Lab8.1/src/Lab8.1.cpp
@@ -0,0 +1,211 @@
+/*
+===============================================================================
+ Name        : main.c
+ Author      : $(author)
+ Version     :
+ Copyright   : $(copyright)
+ Description : main definition
+===============================================================================
+ */
+
+#if defined (__USE_LPCOPEN)
+#if defined(NO_BOARD_LIB)
+
+#include "chip.h"
+#else
+#include "board.h"
+#endif
+#endif
+
+#include <cr_section_macros.h>
+#include <stdlib.h>
+#include <time.h>
+
+// TODO: insert other include files here
+#include <string>
+// TODO: insert other definitions and declarations here
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+#include "queue.h"
+#include "ITM_write.h"
+#include "DigitalIoPin.h"
+
+
+
+
+/*****************************************************************************
+ * Private types/enumerations/variables
+ ****************************************************************************/
+
+/*****************************************************************************
+ * Public types/enumerations/variables
+ ****************************************************************************/
+
+/*****************************************************************************
+ * Private functions
+ ****************************************************************************/
+
+/* Sets up system hardware */
+SemaphoreHandle_t sbPIN;
+QueueHandle_t pinQ;
+extern "C" {
+void PIN_INT0_IRQHandler(void)
+{
+	// This used to check if a context switch is required
+	portBASE_TYPE xHigherPriorityWoken = pdFALSE;
+	//Chip_PININT_ClearFallStates(LPC_GPIO_PIN_INT, PININTCH0);
+	int pin = 1;
+	xQueueSendFromISR(pinQ, &pin, &xHigherPriorityWoken);
+	// End the ISR and (possibly) do a context switch
+	Chip_PININT_ClearIntStatus(LPC_GPIO_PIN_INT, PININTCH0);
+	portEND_SWITCHING_ISR(xHigherPriorityWoken);
+}
+
+void PIN_INT1_IRQHandler(void)
+{
+
+	// This used to check if a context switch is required
+	portBASE_TYPE xHigherPriorityWoken = pdFALSE;
+	//Chip_PININT_ClearFallStates(LPC_GPIO_PIN_INT, PININTCH0);
+	int pin = 2;
+	xQueueSendFromISR(pinQ, &pin, &xHigherPriorityWoken);
+	// End the ISR and (possibly) do a context switch
+	Chip_PININT_ClearIntStatus(LPC_GPIO_PIN_INT, PININTCH1);
+	portEND_SWITCHING_ISR(xHigherPriorityWoken);
+}
+void PIN_INT2_IRQHandler(void)
+{
+
+	// This used to check if a context switch is required
+	portBASE_TYPE xHigherPriorityWoken = pdFALSE;
+	//Chip_PININT_ClearFallStates(LPC_GPIO_PIN_INT, PININTCH0);
+	int pin = 3;
+	xQueueSendFromISR(pinQ, &pin, &xHigherPriorityWoken);
+	// End the ISR and (possibly) do a context switch
+	Chip_PININT_ClearIntStatus(LPC_GPIO_PIN_INT, PININTCH2);
+	portEND_SWITCHING_ISR(xHigherPriorityWoken);
+}
+
+}
+
+
+static void prvSetupHardware(void)
+{
+	SystemCoreClockUpdate();
+	Board_Init();
+	// initialize RIT (= enable clocking etc.)
+	Chip_RIT_Init(LPC_RITIMER);
+	Chip_GPIO_Init(LPC_GPIO);
+	Chip_PININT_Init(LPC_GPIO_PIN_INT);
+
+
+	Chip_PININT_SetPinModeEdge(LPC_GPIO_PIN_INT, PININTCH0);
+	Chip_PININT_SetPinModeEdge(LPC_GPIO_PIN_INT, PININTCH1);
+	Chip_PININT_SetPinModeEdge(LPC_GPIO_PIN_INT, PININTCH2);
+
+	Chip_PININT_EnableIntLow(LPC_GPIO_PIN_INT, PININTCH0);
+	Chip_PININT_EnableIntLow(LPC_GPIO_PIN_INT, PININTCH1 );
+	Chip_PININT_EnableIntLow(LPC_GPIO_PIN_INT, PININTCH2);
+
+	DigitalIoPin sw1(0, 17, DigitalIoPin::pullup, true);
+	DigitalIoPin sw2(1, 11, DigitalIoPin::pullup, true);
+	DigitalIoPin sw3(1, 9, DigitalIoPin::pullup, true);
+
+	Chip_INMUX_PinIntSel(0, 0, 17);
+	Chip_INMUX_PinIntSel(1, 1, 11);
+	Chip_INMUX_PinIntSel(2, 1, 9);
+
+}
+
+void EnablePinINT_NVIC()
+{
+	NVIC_EnableIRQ(PIN_INT0_IRQn);
+	NVIC_EnableIRQ(PIN_INT1_IRQn);
+	NVIC_EnableIRQ(PIN_INT2_IRQn);
+
+
+	// set the priority level of the interrupt
+	// The level must be equal or lower than the maximum priority specified in FreeRTOS config
+	// Note that in a Cortex-M3 a higher number indicates lower interrupt priority
+	NVIC_SetPriority(PIN_INT0_IRQn, configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY + 1 );
+	NVIC_SetPriority(PIN_INT1_IRQn, configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY + 1 );
+	NVIC_SetPriority(PIN_INT2_IRQn, configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY + 1 );
+}
+
+static void Button_Task(void *pvParameters)
+{
+	EnablePinINT_NVIC();
+	int buff = 0;
+	int temp = 0;
+	int cnt = 0;
+	char bt1[] = "Button ";
+	char pr[] = " was pressed ";
+	char tm[] = " times\r\n";
+	bool btnS = false;
+
+	while(1)
+	{
+
+		if(xQueueReceive(pinQ, &buff, portMAX_DELAY))
+		{
+			if(temp == buff)
+			{
+				cnt++;
+			}
+
+			else
+			{
+
+				Board_UARTPutSTR(bt1); Board_UARTPutSTR(std::to_string(temp).c_str());
+				Board_UARTPutSTR(pr); Board_UARTPutSTR(std::to_string(cnt).c_str()); Board_UARTPutSTR(tm);
+				cnt = 1;
+				temp = buff;
+			}
+		}
+
+
+
+	}
+}
+
+
+/*****************************************************************************
+ * Public functions
+ ****************************************************************************/
+
+/* the following is required if runtime statistics are to be collected */
+extern "C" {
+
+void vConfigureTimerForRunTimeStats( void ) {
+	Chip_SCT_Init(LPC_SCTSMALL1);
+	LPC_SCTSMALL1->CONFIG = SCT_CONFIG_32BIT_COUNTER;
+	LPC_SCTSMALL1->CTRL_U = SCT_CTRL_PRE_L(255) | SCT_CTRL_CLRCTR_L; // set prescaler to 256 (255 + 1), and start timer
+}
+
+}
+/* end runtime statictics collection */
+
+/**
+ * @brief	main routine for FreeRTOS blinky example
+ * @return	Nothing, function should not exit
+ */
+int main(void)
+{
+
+	prvSetupHardware();
+ 	pinQ = xQueueCreate(20, sizeof(int));
+
+	xTaskCreate(Button_Task, "Button_Task",
+			configMINIMAL_STACK_SIZE + 128, NULL, (tskIDLE_PRIORITY + 1UL),
+			(TaskHandle_t *) NULL);
+
+
+	/* Start the scheduler */
+	vTaskStartScheduler();
+
+	/* Should never arrive here */
+	return 1;
+}
+
